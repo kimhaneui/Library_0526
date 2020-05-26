b9cc3741630bdd8e8288ede745025506288a3f7b
kimhaneui <kimhaneui2@naver.com>
2020-05-26 17:47:52 +0900

    Glide로 이미지 띄우기

--- app/src/main/java/com/example/library_0526/MainActivity.java
+++ app/src/main/java/com/example/library_0526/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.example.library_0526.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
@@ -26,6 +27,9 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+//인터넷에 있는 이미지 불러오기 => 인터넷 연결 권한 필요
+        Glide.with(mContext).load("https://lh3.googleusercontent.com/proxy/YjKxcyO51y75l7D3RaxpybqWaHe3NM_J7vwqKZp_iXtU42vdYbvIG66g-nTyrSwxhXVgvyOaCU06YqIgBmwLfsFciazJByI9vzFRAOujd7OayRaECBiqsToJxaBAxpwXV7-IJ1-FuNxPlq9-qLzaAPZ4eLYAgH3arHzLjtZOg-ku8Q").into(binding.profileId);
 
     }
 }
